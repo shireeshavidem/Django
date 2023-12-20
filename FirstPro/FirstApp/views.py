d75ea2045b5a4f0ada9f63e3ee0b95f4a26175b9
shireeshavidem <shireesha.marolix@gmail.com>
2023-12-20 13:12:56 +0530

    first project

--- FirstPro/FirstApp/views.py
+++ FirstPro/FirstApp/views.py
@@ -1,8 +1,21 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Employee
 # Create your views here.
 # def greet(request):
 #     return HttpResponse("Hello Team")
 
+# def greet(request):
+#     return render(request,"index.html")
+
+
 def greet(request):
+    if request.method == 'POST':
+        ename = request.POST['name'],
+        eid = request.POST['id']
+        edomain = request.POST['domain'],
+        ecompany = request.POST['company'],
+        emp = Employee(name=ename ,id=eid ,domain=edomain ,company=ecompany)
+        emp.save()
     return render(request,"index.html")
